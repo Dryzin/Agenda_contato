e5324fcac42ba1c7308c354e2911432a21e5ac4f
Dryzin <endrioemanuel11@gmail.com>
2022-06-27 11:29:38 -0300

    tentativas

--- agenda.py
+++ agenda.py
@@ -9,11 +9,19 @@ class Agenda:
 
     def listar_contatos(self):
         for i in range(len(self.listaContatos)):
-            print('\nCódigo -',self.listaContatos[i].cod,'\nNome -', self.listaContatos[i].nome,'/n')
+            print('\nCódigo -',self.listaContatos[i].cod,'\nNome -', self.listaContatos[i].nome,'\n Telefone -',self.listaContatos[i].telefone,'\n')
 
     def mudar_contato(self):
-        b = input("buscar o contato por nome para mudar")
-        for i in range(len(self.listaContatos)):
-            if b == self.listaContatos[i].cod:
-                self.listaContatos[i].telefone = input('Digite o novo número')
+        b = input("Informe o código do número de mudança: ")
+        c = input("Mudar Nome -[1]\nMudar Núemro -[2]\n")
 
+        for i in range(len(self.listaContatos)):
+            
+            if b == self.listaContatos[i].cod and c == 1:
+                    self.listaContatos[i].nome = input('Digite o novo nome: ')
+
+            elif b == self.listaContatos[i].cod and c == 2:
+                    self.listaContatos[i].telefone = input('Digite o novo número: ')
+
+            else:
+                pass
